f72612de055e7fbeea511e22203395c3759db2f2
oofeyang22 <olaitanferanmi41@gmail.com>
2026-03-07 08:24:25 +0100

    latest update

--- vite-project/src/components/HistoricalSite.jsx
+++ vite-project/src/components/HistoricalSite.jsx
@@ -5,14 +5,13 @@ const HistoricalSite = ({historicalsite}) => {
 
   return (
     <article>
-        <h2 className='site'>{historicalsite.site}</h2>
+
         <div class ="center">
           <img src={historicalsite.coverImage.url} alt='weird' />
         </div>
 
         <div className='published-time'>
-            <i className='far fa-clock'></i>
-            <span>{new Date(historicalsite.updatedAt).toDateString()}</span>
+          <h2 style={{color: "white"}}>{historicalsite.site}</h2>
         </div>
         <div className='content' dangerouslySetInnerHTML={{__html: historicalsite.content.html}}/>
     </article>
